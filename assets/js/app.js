3a128a771720fc425ac12eaa68692075f04d3755
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-06-25 00:55:51 -0500

    cleaned up code, added comments

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,99 +13,70 @@ firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
 
-// function something() {
-//     database.ref().set({ friend: "armando" })
-// };
-
-
-
 $("#trainForm").on("submit", function (event) {
     event.preventDefault();
-
+    // variables to check if the form is filled in valid
     var regexTime = /^\d{1,2}:\d{2}([ap]m)?$/;
     var trainTime = $("#trainForm input[name = firstTrain]").val().trim();
-    // var matchTime = trainTime.match(regexTime);
     var trainName;
     var destination;
     var firstTrain;
     var frequency;
-
+    // train name must contain 3 or more letters
     if ($("#trainForm input[name=trainName]").val().trim().length > 2) {
-
         trainName = $("#trainForm input[name=trainName]").val().trim();
     } else {
         alert("Train name should be 3 letters or more")
     }
+    // train destination must contain 3 or more letters
     if ($("#trainForm input[name=destination]").val().trim().length > 2) {
         destination = $("#trainForm input[name=destination]").val().trim();
     }
     else {
         alert("Destination should be 3 letters or more")
     }
+    // trainTime must be in the form of hh:mm
     if (trainTime != "" && !trainTime.match(regexTime)) {
         alert("Time must be in hh:mm")
-
     }
     else {
         firstTrain = trainTime;
     }
+    // train name must be a number
     if (isNaN($("#trainForm input[name=frequency]").val().trim())) {
         alert("Frequency must be a number")
     }
     else {
         frequency = $("#trainForm input[name=frequency]").val().trim()
     }
-
-
+    // object to store all the information collected from the form
     var trainInfo = {
         name: trainName,
         destination: destination,
         first: firstTrain,
         frequency: frequency
     }
-
+    // pushing the object to firebase
     database.ref().push(trainInfo)
 })
 
-
-
+// retrieving information from firebase
 database.ref().on('child_added', function (response) {
-    console.log(response.val())
     // making frequency a var to use in moment.js
     var freq = response.val().frequency;
-    console.log("frequency: " + freq);
-    // grabbing the current time
-    var currentTime = moment();
-    console.log("current Time: " + moment(currentTime).format("hh:mm"));
     // first Train 
     var firstTrain = response.val().first;
-    console.log("first Train: " + firstTrain);
     // making sure first train was earlier than the current time
     var firstTrainEarly = moment(firstTrain, "HH:mm").subtract(1, "years");
-    console.log("1 year ago: " + firstTrainEarly);
     // difference between the first train and the current time
     var difference = moment().diff(moment(firstTrainEarly), "minutes");
-    console.log("Difference in time " + difference);
     // Time apart
     var timeApart = difference % freq;
-    console.log(timeApart);
     // next Train
-    var nextTrain = freq - timeApart;
-    console.log("Minutes till next train " + nextTrain);
-
+    var nextTrain = freq - timeApart;;
     // arrival time
     var arrival = moment().add(nextTrain, "minutes");
-    console.log("Arrival " + moment(arrival).format("hh:mm"));
-
     // append schedule
-
     $("#schedule").append("<tr>" + "<td>" + response.val().name + "</td>" + "<td>" + response.val().destination + "</td>" + "<td>" +
-    response.val().frequency + "</td>" + "<td>" + moment(arrival).format('hh:mm') + "</td>" + "<td>" + nextTrain + "</td>" + "</tr>");
-
-    //use moment js to calculate arrival 
-
-    //table
-    //tr
-    //td 
-    //empty-div
+        response.val().frequency + "</td>" + "<td>" + moment(arrival).format('hh:mm') + "</td>" + "<td>" + nextTrain + "</td>" + "</tr>");
 })
