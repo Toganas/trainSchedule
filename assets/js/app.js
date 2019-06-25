d4d191d1de804d186e8fa845d685aba002c1c519
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-06-24 23:51:11 -0500

    added moment js, added next arrival and minutes away.  added code using moment js to get current time as well as figure out when the next arrival would be and how far away it is.

--- assets/js/app.js
+++ assets/js/app.js
@@ -71,29 +71,35 @@ $("#trainForm").on("submit", function (event) {
 
 database.ref().on('child_added', function (response) {
     console.log(response.val())
+    // making frequency a var to use in moment.js
+    var freq=response.val().frequency;
+    console.log("frequency: " + freq);
+    // grabbing the current time
+    var currentTime=moment();
+    console.log("current Time: " + moment(currentTime).format("hh:mm"));
+    // first Train 
+    var firstTrain=response.val().first;
+    console.log("first Train: "+firstTrain);
+    // making sure first train was earlier than the current time
+    var firstTrainEarly = moment(firstTrain, "HH:mm").subtract(1,"years");
+    console.log("1 year ago: "+firstTrainEarly);
+    // difference between the first train and the current time
+    var difference = moment().diff(moment(firstTrainEarly), "minutes");
+    console.log("Difference in time "+difference);
+    // Time apart
+    var timeApart = difference % freq;
+    console.log(timeApart);
+    // next Train
+    var nextTrain = freq - timeApart;
+    console.log("Minutes till next train "+nextTrain);
 
-    // Attempt to append schedule
-    // $("<td>").text(response.val().name);
-    $("#schedule").append("<tr>" + "<td>" + response.val().name + "</td>" + "<td>" + response.val().destination + "</td>" + "<td>" + response.val().frequency + "</td>" + "</tr>")
+    // arrival time
+    var arrival = moment().add(nextTrain, "minutes");
+    console.log("Arrival "+moment(arrival).format("hh:mm"));
 
-    // Does not work
-    // for (var i = 0; i < response.length; i++) {
-    //     console.log(response[i].name);
-    // $("#schedule").html(`
-    // <tr>
-    // <th scope="row">${name}</th>
-    // <td>${destination}</td>
-    // <td>${frequency}</td>
-    // </tr>
-    //  `)
-    // }
-    // Will a forEach function work instead?
-    // response.val().forEach(myFunction);
-
-    // function myFunction(name, trainName){
-
-
-    // }
+    // append schedule
+    
+    $("#schedule").append("<tr>" + "<td>" + response.val().name + "</td>" + "<td>" + response.val().destination + "</td>" + "<td>" + response.val().frequency + "</td>" + "<td>" + moment(arrival).format('hh:mm')+"</td>"+"<td>"+nextTrain+"</td>"+ "</tr>");
 
     //use moment js to calculate arrival 
 
